3a9809bea24cbb76303937e82d0e3fe33e947a70
Drew Verwiel <Verwiel.Code@gmail.com>
2019-03-28 10:51:03 -0600

    fixed issues

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function inputForm (userName) {
     document.getElementById('name-card-name').innerHTML=userName;
     input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
-   event.preventDefault();
-   document.getElementById("enterButton").click();
+    event.preventDefault();
+    document.getElementById("enterButton").click();
     }
   })
 };
